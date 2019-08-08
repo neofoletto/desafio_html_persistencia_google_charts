456bfd5fce3999a289ec247fb1f120f7b51aa6cd
angelo foletto <angelofoletto@hotmail.com>
2019-08-08 19:36:51 -0300

    Iniciando repositório
    Commit de configuração.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # desafio_html_persistencia_google_charts
+
+Usando o recursos disponíveis no Google Charts crie uma aplicação Java que gere uma arquivo .HTML com os dados do gráfico dos valores informados.
---
+++ src/code/Main.java
@@ -0,0 +1,29 @@
+/*
+ *                     GNU GENERAL PUBLIC LICENSE
+ *                        Version 3, 29 June 2007
+ *
+ *  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ *  Everyone is permitted to copy and distribute verbatim copies
+ *  of this license document, but changing it is not allowed.
+ *
+ *                             Preamble
+ *
+ *   The GNU General Public License is a free, copyleft license for
+ * software and other kinds of works.
+ */
+
+package code;
+
+import java.util.Scanner;
+
+/**
+ * @author neo
+ * @create 08/08/2019
+ */
+public class Main {
+  public static void main(String[] args) {
+    Scanner imput = new Scanner(System.in);
+
+
+  }
+}
